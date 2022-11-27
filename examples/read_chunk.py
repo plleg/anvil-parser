501f0e5ab1b184717c89315080b16993056e84dd
plleg <montgomery@mftidwell.com>
2022-11-27 16:02:59 -0600

    test

---
+++ examples/read_chunk.py
@@ -0,0 +1,19 @@
+"""
+Generates a image of the top view of a chunk
+Needs a textures folder with a block folder inside
+"""
+import sys
+
+region = "D:\Dockyard\LCWorld\world\region\r.0.0.mca"
+chx = 0
+chz = 0
+
+import os
+from PIL import Image
+import _path
+#import anvil
+
+chunk = anvil.Chunk.from_region(region, chx, chz)
+
+print("read chunk")
+
